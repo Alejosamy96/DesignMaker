cea1c6bb03c2fc5b58be5676e98bd580c157deb6
Alejo <alejandrolopezpipos@gmail.com>
2025-09-25 11:47:05 -0500

    Inserte mensaje

--- JS/Script-Q&A.js
+++ JS/Script-Q&A.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const items = document.querySelectorAll(".conjunto-container");
+  const QYAcontainer = document.querySelector(".QYA .QYA-container");
 
   items.forEach((item) => {
     const preguntaContainer = item.querySelector(".pregunta-container");
@@ -7,6 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const respuesta = item.querySelector(".respuesta");
 
     preguntaContainer.addEventListener("click", () => {
+      let algunoActivo = false;
+
       items.forEach((i) => {
         if (i !== item) {
           i.classList.remove("activo");
@@ -14,15 +17,25 @@ document.addEventListener("DOMContentLoaded", () => {
           if (r) r.style.maxHeight = null;
         }
       });
+
       if (item.classList.contains("activo")) {
         item.classList.remove("activo");
         respuestaContainer.style.maxHeight = null;
       } else {
         item.classList.add("activo");
         respuestaContainer.style.maxHeight = respuesta.scrollHeight + "px";
+        algunoActivo = true;
+      }
+
+      // Ajustar margen inferior del contenedor
+      if (algunoActivo) {
+        QYAcontainer.style.marginBottom = "-210px"; // lo ajustas según lo que necesites
+      } else {
+        QYAcontainer.style.marginBottom = "-260px"; // valor original que tienes en tu CSS
       }
     });
   });
+
   window.addEventListener("resize", () => {
     items.forEach((item) => {
       if (item.classList.contains("activo")) {
