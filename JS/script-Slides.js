755a130d84dedd31ffcffe6c120973944fe8fff3
Alejo <alejandrolopezpipos@gmail.com>
2025-09-28 21:06:48 -0500

    Todo funciona como debería, falta únicamente 2 secciones, animo hpta

--- JS/script-Slides.js
+++ JS/script-Slides.js
@@ -1,8 +1,3 @@
 const pista = document.querySelector('.seccion-deslizar-pista');
-  const logos = pista.innerHTML;
-  const repeticiones = 5; // 👈 aquí pones cuántas veces quieres repetir el set
-  let contenido = '';
-  for (let i = 0; i < repeticiones; i++) {
-    contenido += logos;
-  }
-  pista.innerHTML = contenido;
+const logos = pista.innerHTML;
+pista.innerHTML += logos; // Duplicar logos para animación fluida
